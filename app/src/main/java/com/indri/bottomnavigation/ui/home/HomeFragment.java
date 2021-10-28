ebd40468a9276de0686e72621cb9229771cccc18
indri12346 <66579038+indri12346@users.noreply.github.com>
2021-10-28 22:27:39 +0700

    commit

---
+++ app/src/main/java/com/indri/bottomnavigation/ui/home/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.indri.bottomnavigation.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.indri.bottomnavigation.R;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
